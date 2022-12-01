677d9f1de8ef259b8b69e33fd5162def9fe0e6bc
Isaac <zac.araujo2001@gmail.com>
2022-12-01 00:40:17 -0300

    black optimization

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import requests
 import bs4
 from fake_useragent import UserAgent
 
-MOVIES_URL = 'https://www.coolgenerator.com/random-movie-generator'
+MOVIES_URL = "https://www.coolgenerator.com/random-movie-generator"
+
 
 def get_imdb_link(movie_title: str) -> str:
     """_summary_
@@ -14,20 +15,20 @@ def get_imdb_link(movie_title: str) -> str:
         str: Imdb Url of the movie
     """
     url = f'https://www.imdb.com/find?q={movie_title.replace(" ", "+")}'
-    ua = UserAgent()
     try:
         for _ in range(2):
-            response = requests.get(url, headers={"User-Agent": ua.firefox})
+            response = requests.get(url, headers={"User-Agent": UserAgent().firefox})
             if response.ok:
                 break
-        soup = bs4.BeautifulSoup(response.content, 'html.parser')
-        imdblink = soup.find('a', {'class': 'ipc-metadata-list-summary-item__t'})
-        url_imdb = 'https://www.imdb.com' + imdblink.attrs['href']
-            
+        soup = bs4.BeautifulSoup(response.content, "html.parser")
+        imdb_link = soup.find("a", {"class": "ipc-metadata-list-summary-item__t"})
+        url_imdb = "https://www.imdb.com" + imdb_link.attrs["href"]
+
     except Exception:
-        return ''
-    
+        return ""
+
     return url_imdb
+
 
 def get_movie_images(movies: bs4.element.Tag) -> list:
     """_summary_
@@ -39,10 +40,11 @@ def get_movie_images(movies: bs4.element.Tag) -> list:
         list: List of the movies images
     """
     images = []
-    for imgs in movies.select('img'):
-        src = 'https:' + imgs.get('src')
+    for imgs in movies.select("img"):
+        src = "https:" + imgs.get("src")
         images.append(src)
     return images
+
 
 def get_movies(num_movies: int) -> None:
     """_summary_
@@ -53,32 +55,31 @@ def get_movies(num_movies: int) -> None:
     count = 0
     while True:
         response = requests.get(MOVIES_URL, headers={"UserAgent": UserAgent().chrome})
-        soup = bs4.BeautifulSoup(response.content, 'html.parser')
+        soup = bs4.BeautifulSoup(response.content, "html.parser")
 
-        movies_ul = soup.find('ul', {'class': 'list-unstyled content-list'})
+        movies_ul = soup.find("ul", {"class": "list-unstyled content-list"})
         images = get_movie_images(movies_ul)
 
         for idx, movie in enumerate(movies_ul.contents):
-            if movie.name != 'li':
+            if movie.name != "li":
                 break
             if count >= num_movies:
                 return
-            title = (movie.contents[2].text).replace('(', ' (')
-            genre = (movie.contents[3].contents[1].text).replace(',', ', ')
-            rate = (movie.contents[3].contents[3].text)
+            title = (movie.contents[2].text).replace("(", " (")
+            genre = (movie.contents[3].contents[1].text).replace(",", ", ")
+            rate = movie.contents[3].contents[3].text
             imdb = get_imdb_link(title)
-                
-            print(f'TITLE: {title}')
-            print(f'GENRE: {genre}')
-            print(f'RATING: {float(rate):.1f}')
-            print(f'IMDb: {imdb}')
-            print(f'IMAGE URL: {images[idx]}\n')
+
+            print(f"TITLE: {title}")
+            print(f"GENRE: {genre}")
+            print(f"RATING: {float(rate):.1f}")
+            print(f"IMDb: {imdb}")
+            print(f"IMAGE URL: {images[idx]}\n")
             count += 1
-    
+
 
 if __name__ == "__main__":
 
-    num_movies = int(input('Enter a number of movies: ').strip())
-    print('\n🎥 Picking Random movies ...\n')
+    num_movies = int(input("Enter a number of movies: ").strip())
+    print("\n🎥 Picking Random movies ...\n")
     get_movies(num_movies)
-    
